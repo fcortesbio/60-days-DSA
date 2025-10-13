3bf72735f77203e6c63630a6a4ae26d9c8ee13d1
fcortesbio <fcortesbio@gmail.com>
2025-10-13 11:28:39 -0500

    temp: initial Java implementation with basic structure
    - Add basic Solution.java with naive brute force subarray sum algorithm
    - Fix syntax error (missing semicolon) in implementation
    - Update date to October 2025
    - Add comprehensive Java .gitignore patterns (.class, .jar, IDE files)
    - Basic structure ready for further algorithm implementations
    
    WIP: Additional algorithms and testing to be implemented

--- 01-subarray_sums_and_range_queries/Solution.java
+++ 01-subarray_sums_and_range_queries/Solution.java
@@ -1,20 +1,45 @@
 /**
  * Day 1: Subarray Sums & Range Queries
  * Java Implementation
- * 
+ *
  * Author: Andres
- * Date: October 2024
+ * Date: October 2025
  */
 
 public class Solution {
-    
+
     // TODO: Implement solutions for both problems
-    
+
     /**
      * Main method to run tests
      */
     public static void main(String[] args) {
         // TODO: Add test cases and run solutions
         System.out.println("Day 1: Ready to implement!");
+    }
+
+    public class SubarraySum {
+        /**
+         * Calculates the sum of all subarray sums using a naive, triple-loop approach
+         * @param arr The input array of integers
+         * @return The sum of all possible subarray sums
+         */
+        public static int naiveBruteForce(int[] arr){
+            int totalSum = 0;
+            int n = arr.length;
+
+            // first loop sets the starting point of the subarray
+            for (int i = 0; i < n ; i++){
+                // second loop sets the ending point of the subarray
+                for (int j = i; j < n; j++){
+                    // third loop calculates the sum of the subarray
+                    for (int k = i; k <= j; k++){
+                        totalSum += arr[k];
+                    }
+                }
+            }
+            return totalSum;
+        }
+
     }
 }
